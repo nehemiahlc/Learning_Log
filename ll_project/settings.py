b7d20662b2d72241763e6fee37a2e7331228cc51
Nemo <nehemiahchandler@Nehemiahs-MacBook-Pro.local>
2024-03-21 20:38:41 -0500

    excluding uncessary files

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps. 
     'learning_logs',
+    'accounts',
     
     # Default django apps.
     'django.contrib.admin',
@@ -125,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings. 
+
+LOGIN_REDIRECT_URL = 'learning_logs:index'
+LOGOUT_REDIRECT_URL = 'learning_logs:index'
+LOGIN_URL = 'accounts:login'
